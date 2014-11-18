fb9ccdb36a3e79d1771b29317f5e205ac9e6deee
leif <leif@beansmile.com>
2014-11-18 23:18:08 +0800

    add index/new/show page for team

--- db/schema.rb
+++ db/schema.rb
@@ -104,9 +104,6 @@ ActiveRecord::Schema.define(version: 20141115150653) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.integer  "failed_attempts",        default: 0,  null: false
-    t.string   "unlock_token"
-    t.datetime "locked_at"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
@@ -122,6 +119,5 @@ ActiveRecord::Schema.define(version: 20141115150653) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
 
 end
